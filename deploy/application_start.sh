d44495b6e638f5b1349a0ddcc9606f62c3c73bda
Damian Esteban <damian.esteban@gmail.com>
2017-09-04 00:02:33 -0400

    Clean up

--- deploy/application_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#pkill -f nodeoset -e
-set -e
-export PORT=80
-
-cd /home/ec2-user/server
-export PORT=80
-forever start server.js
--- deploy/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo DOWNLOADING NODE
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum install -y nodejs
-echo INSTALLING NODE
-npm install forever -g
