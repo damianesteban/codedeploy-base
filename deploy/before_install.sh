f6820c1d62cbe19611b199353d959b907a869d22
Damian Esteban <damian.esteban@gmail.com>
2017-09-03 21:35:45 -0400

    More tweaks

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo DOWNLOADING NODE
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y nodejs
+echo INSTALLING NODE
 npm i -g pm2@2.4.3
+npm install
+echo INSTALLING APP 
